e7f029e6a6c5168bb00dea55e5b5f46262e41a90
emobebra <uspenska.e.b_ipz24@rcit.ukr.education>
2026-09-22 21:45:37 +0300

    lab1final

--- lab1v3/Program.cs
+++ lab1v3/Program.cs
@@ -54,8 +54,6 @@ namespace lab1v3
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-
             Student st1 = new Student("Федчук Ангеліна", 101, 92.5);
             Student st2 = new Student("Ніколаєв Максим", 102, 85.0);
             Student st3 = new Student("Бзіта Олена", 103, 78.4);
